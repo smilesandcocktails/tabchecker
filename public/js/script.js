93225a6e6f42996d57722de4c0c837d33aa3d6ff
Cara Chew <carachew@gmail.com>
2017-04-20 03:50:47 +0800

    working on css front pages

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+
+// var viewallbtn = document.querySelector('#viewallbtn')
+//
+// viewallbtn.addEventListener('click', function () {
+//   var body = document.querySelector('body')
+//   var h1 = document.querySelector('h1')
+//
+//   h1.textContent = 'EVERYONE\'S TABS'
+//   h1.appendChild(body)
+//
+//
+// })
